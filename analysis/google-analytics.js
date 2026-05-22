3c8479c31c4ba2ff7daf998544cb53a5ff2b0232
Advait <advaitpatel69@gmail.com>
2026-05-22 17:36:06 +0530

    Create analysis/google-analytics.js

---
+++ analysis/google-analytics.js
@@ -0,0 +1,8 @@
+<!-- Google tag (gtag.js) -->
+async src="https://www.googletagmanager.com/gtag/js?id=G-B0M63KX41H"
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B0M63KX41H');
