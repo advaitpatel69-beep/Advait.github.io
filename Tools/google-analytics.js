83e6527d43b68dd0ce2e7383c9aab2b7266d49c4
Advait <advaitpatel69@gmail.com>
2026-05-26 08:49:35 +0530

    Update google-analytics.js

--- Tools/google-analytics.js
+++ Tools/google-analytics.js
@@ -9,3 +9,14 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-B0M63KX41H');
+
+
+
+// clarity code 
+
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "wj65xd3z7a");
+
